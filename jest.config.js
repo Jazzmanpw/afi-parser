1f24c302e4bca41e4a078ca1339adab7c8b4a62f
hummer__revolver <jazzmanpw@mail.ru>
2020-04-10 13:27:57 +0300

    Initial commit
    Flow, Jest and Babel to bring them all together are installed

---
+++ jest.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  moduleFileExtensions: ['js', 'mjs'],
+  testEnvironment: 'node',
+  testEnvironmentOptions: { experimentalModules: true },
+  transform: { '\\.m?js$': 'babel-jest' },
+};
