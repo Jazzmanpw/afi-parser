e35acbd8a1049fe6d26072e9d9faf61a4dd309aa
hummer__revolver <jazzmanpw@mail.ru>
2020-05-06 23:24:29 +0300

    export rep from basicParsers

--- src/basicParsers/index.js
+++ src/basicParsers/index.js
@@ -1,5 +1,5 @@
 // @flow
 export { reg, text } from './atomic';
-export { seq, union } from './compound';
+export { rep, seq, union } from './compound';
 
 export type { ParserType, ParserResultType } from './types';
